0e3adecbd478fc8d896c8692975dd9a3fcac940e
Viktor <a.biktop@gmail.com>
2025-12-06 16:19:13 +0300

    fix makefile

--- luci-app-update-podkop/luasrc/controller/update_podkop.lua
+++ luci-app-update-podkop/luasrc/controller/update_podkop.lua
@@ -1,37 +1,48 @@
+local uci = require("uci")
+local http = require("luci.http")
+local util = require("luci.util")
+local sys = require("luci.sys")
+
 module("luci.controller.update_podkop", package.seeall)
 
 function index()
-    entry({"admin","services","update_podkop"}, call("page_index"), "Update Podkop", 20).dependent=false
+    entry({"admin", "services", "update_podkop"}, call("page_index"), _("Update Podkop"), 20).dependent = false
 end
 
 function page_index()
-    local uci = require("uci").cursor()
-    local lhttp = require("luci.http")
-
-    if lhttp.formvalue("save") then
-        local url = lhttp.formvalue("url") or ""
-        local token = lhttp.formvalue("token") or ""
-        uci:set("update-podkop","settings","url",url)
-        uci:set("update-podkop","settings","token",token)
-        uci:commit("update-podkop")
+    local cursor = uci.cursor()
+    
+    if http.formvalue("save") then
+        local url = http.formvalue("url") or ""
+        local token = http.formvalue("token") or ""
+        
+        cursor:set("update-podkop", "settings", "url", url)
+        cursor:set("update-podkop", "settings", "token", token)
+        cursor:commit("update-podkop")
     end
 
-    local url = uci:get("update-podkop","settings","url") or ""
-    local token = uci:get("update-podkop","settings","token") or ""
+    local url = cursor:get("update-podkop", "settings", "url") or ""
+    local token = cursor:get("update-podkop", "settings", "token") or ""
 
-    lhttp.prepare_content("text/html")
-    lhttp.write([[
-      <h2>Update Podkop</h2>
-      <form method="post">
-        URL: <input type="text" name="url" value="]]..url..[["><br>
-        Token: <input type="password" name="token" value="]]..token..[["><br>
-        <input type="submit" name="save" value="Сохранить"><br><br>
-        <input type="submit" name="run" value="Запустить update-podkop">
-      </form>
-    ]])
+    http.prepare_content("text/html")
+    http.write(string.format([[
+<!DOCTYPE html>
+<html>
+<head><title>Update Podkop</title></head>
+<body>
+  <h2>Update Podkop</h2>
+  <form method="post">
+    <label>URL: <input type="text" name="url" value="%s" size="50"></label><br>
+    <label>Token: <input type="password" name="token" value="%s" size="50"></label><br><br>
+    <input type="submit" name="save" value="Сохранить">
+    <input type="submit" name="run" value="Запустить">
+  </form>
+]], util.pcdata(url), util.pcdata(token)))
 
-    if lhttp.formvalue("run") then
-        local out = io.popen("update-podkop 2>&1"):read("*a")
-        lhttp.write("<pre>"..out.."</pre>")
+    if http.formvalue("run") then
+        local result = sys.exec("update-podkop 2>&1")
+        http.write("<h3>Результат:</h3><pre>" .. util.pcdata(result) .. "</pre>")
     end
+    
+    http.write("</body></html>")
 end
